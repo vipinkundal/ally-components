91910101efdde60db655f226722e796da72597db
vipinkundal <vipinkundal15@gmail.com>
2025-12-09 14:51:46 +0530

    ARIA positioning that ignores disabled options

--- src/components/ally/AllySelect.vue
+++ src/components/ally/AllySelect.vue
@@ -81,6 +81,20 @@ const describedBy = computed(() => {
   const uniqueIds = Array.from(new Set(ids)).filter(id => id !== labelId.value);
   return uniqueIds.length ? uniqueIds.join(' ') : undefined;
 });
+
+// Provide ARIA positioning that ignores disabled options (and the hidden placeholder)
+const enabledOptions = computed(() => props.options.filter(option => !option.disabled));
+const ariaSetSize = computed(() => enabledOptions.value.length);
+const optionPositions = computed(() => {
+  const map = new Map<string | number, number>();
+  let position = 1;
+  for (const option of props.options) {
+    if (option.disabled) continue;
+    map.set(option.value, position);
+    position += 1;
+  }
+  return map;
+});
 </script>
 
 <template>
@@ -101,12 +115,15 @@ const describedBy = computed(() => {
         :aria-describedby="describedBy"
         @blur="$emit('blur', $event)"
       >
-        <option value="" disabled aria-hidden="true">{{ placeholder }}</option>
+        <option value="" disabled hidden aria-hidden="true">{{ placeholder }}</option>
         <option
           v-for="option in options"
           :key="option.value"
           :value="option.value"
           :disabled="option.disabled"
+          :aria-selected="value === option.value ? 'true' : undefined"
+          :aria-setsize="!option.disabled ? ariaSetSize : undefined"
+          :aria-posinset="!option.disabled ? optionPositions.get(option.value) : undefined"
         >
           {{ option.label }}
         </option>
